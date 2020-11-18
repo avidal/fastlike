2f6b3211a1d1682d0b722255ab5d9c030e177c4f
Alex Vidal <alex@heyviddy.com>
2020-11-17 20:35:15 -0600

    Update example wasm to latest fastly dependencies

--- testdata/src/main.rs
+++ testdata/src/main.rs
@@ -3,29 +3,21 @@ use fastly::request::downstream_client_ip_addr;
 use fastly::geo::geo_lookup;
 use fastly::{Body, Error, Request, RequestExt, Response, ResponseExt};
 use fastly::uap_parse;
-use std::convert::TryFrom;
 use serde_json;
 
 const BACKEND: &str = "backend";
 
-// The example server will send any requests for this backend to httpbin.org
-const HTTPBIN: &str = "httpbin";
-
 #[fastly::main]
 fn main(mut req: Request<Body>) -> Result<impl ResponseExt, Error> {
-    if req.headers().contains_key("httpbin-proxy") {
-        return req.send(HTTPBIN)
-    }
-
     match (req.method(), req.uri().path()) {
         (&Method::GET, "/simple-response") => Ok(Response::builder()
             .status(StatusCode::OK)
-            .body(Body::try_from("Hello, world!")?)?
+            .body(Body::from("Hello, world!"))?
         ),
 
         (&Method::GET, "/no-body") => Ok(Response::builder()
             .status(StatusCode::NO_CONTENT)
-            .body(Body::new()?)?),
+            .body(Body::new())?),
 
         (&Method::GET, "/user-agent") => {
             let ua = req.headers().get("user-agent");
@@ -48,26 +40,26 @@ fn main(mut req: Request<Body>) -> Result<impl ResponseExt, Error> {
             };
             Ok(Response::builder()
                .status(StatusCode::OK)
-               .body(Body::try_from(s)?)?)
+               .body(Body::from(s))?)
         },
 
         (&Method::GET, "/append-header") => {
             req.headers_mut().insert("test-header", HeaderValue::from_static("test-value"));
-            req.send(BACKEND)
+            Ok(req.send(BACKEND)?)
         },
 
         (&Method::GET, "/append-body") => {
-            let other = Body::try_from("appended")?;
-            let rw = Response::new(Body::try_from("original\n")?);
+            let other = Body::from("appended");
+            let rw = Response::new(Body::from("original\n"));
             let (mut parts, mut body) = rw.into_parts();
-            body.append(other)?;
+            body.append(other);
             parts.status = StatusCode::OK;
             let rv = Response::from_parts(parts, body);
             Ok(rv)
         },
 
         (&Method::GET, path) if path.starts_with("/proxy") => {
-            req.send(BACKEND)
+            Ok(req.send(BACKEND)?)
         },
 
         (&Method::GET, "/panic!") => {
@@ -77,29 +69,29 @@ fn main(mut req: Request<Body>) -> Result<impl ResponseExt, Error> {
         (&Method::GET, "/geo") => {
             let ip = downstream_client_ip_addr();
             if ip.is_none() {
-                return Ok(Response::builder().status(StatusCode::INTERNAL_SERVER_ERROR).body(Body::new()?)?);
+                return Ok(Response::builder().status(StatusCode::INTERNAL_SERVER_ERROR).body(Body::new())?);
             }
             let geodata = geo_lookup(ip.unwrap()).unwrap();
             Ok(Response::builder()
                 .status(StatusCode::OK)
                 .body(
-                    Body::try_from(
+                    Body::from(
                         serde_json::json!({
                             "as_name": geodata.as_name(),
                         }).to_string()
-                    )?
+                    )
                 )?
             )
         },
 
         // This one is used for example purposes, not tests
         (&Method::GET, path) if path.starts_with("/testdata") => {
-            req.send(BACKEND)
+            Ok(req.send(BACKEND)?)
         },
 
         _ => Ok(Response::builder()
             .status(StatusCode::NOT_FOUND)
-            .body(Body::try_from("The page you requested could not be found")?)?
+            .body(Body::from("The page you requested could not be found"))?
         ),
     }
 }
