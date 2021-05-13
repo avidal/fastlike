a8fdeceda64868f591e1ee47115d9adf9fcc00b1
Alex Vidal <alex@heyviddy.com>
2021-05-13 16:01:36 -0500

    Update rust spec implementation to latest fastly crate

--- specs/testdata/rust/src/main.rs
+++ specs/testdata/rust/src/main.rs
@@ -1,26 +1,25 @@
-use fastly::http::{HeaderValue, Method, StatusCode};
-use fastly::request::downstream_client_ip_addr;
+use fastly::{Request, Response, Body, Error};
+use fastly::http::{Method, StatusCode};
+use fastly::experimental::uap_parse;
 use fastly::geo::geo_lookup;
-use fastly::{Body, Error, Request, RequestExt, Response, ResponseExt};
-use fastly::uap_parse;
 use serde_json;
 
 const BACKEND: &str = "backend";
 
 #[fastly::main]
-fn main(mut req: Request<Body>) -> Result<impl ResponseExt, Error> {
-    match (req.method(), req.uri().path()) {
-        (&Method::GET, "/simple-response") => Ok(Response::builder()
-            .status(StatusCode::OK)
-            .body(Body::from("Hello, world!"))?
+fn main(mut req: Request) -> Result<Response, Error> {
+    match (req.get_method(), req.get_url().path()) {
+        (&Method::GET, "/simple-response") => Ok(Response::new()
+            .with_status(200)
+            .with_body("Hello, world!")
         ),
 
-        (&Method::GET, "/no-body") => Ok(Response::builder()
-            .status(StatusCode::NO_CONTENT)
-            .body(Body::new())?),
+        (&Method::GET, "/no-body") => Ok(Response::new()
+            .with_status(StatusCode::NO_CONTENT)
+        ),
 
         (&Method::GET, "/user-agent") => {
-            let ua = req.headers().get("user-agent");
+            let ua = req.get_header("user-agent");
             let result = match ua {
                 Some(inner) => {
                     uap_parse(inner.to_str()?)
@@ -38,23 +37,20 @@ fn main(mut req: Request<Body>) -> Result<impl ResponseExt, Error> {
                 },
                 Err(_) => { "error".to_string() },
             };
-            Ok(Response::builder()
-               .status(StatusCode::OK)
-               .body(Body::from(s))?)
+            Ok(Response::new()
+               .with_status(200)
+               .with_body(s)
+            )
         },
 
         (&Method::GET, "/append-header") => {
-            req.headers_mut().insert("test-header", HeaderValue::from_static("test-value"));
+            req.set_header("test-header", "test-value");
             Ok(req.send(BACKEND)?)
         },
 
         (&Method::GET, "/append-body") => {
-            let other = Body::from("appended");
-            let rw = Response::new(Body::from("original\n"));
-            let (mut parts, mut body) = rw.into_parts();
-            body.append(other);
-            parts.status = StatusCode::OK;
-            let rv = Response::from_parts(parts, body);
+            let mut rv = Response::from_body("original\n");
+            rv.append_body(Body::from("appended"));
             Ok(rv)
         },
 
@@ -67,20 +63,18 @@ fn main(mut req: Request<Body>) -> Result<impl ResponseExt, Error> {
         },
 
         (&Method::GET, "/geo") => {
-            let ip = downstream_client_ip_addr();
+            let ip = req.get_client_ip_addr();
             if ip.is_none() {
-                return Ok(Response::builder().status(StatusCode::INTERNAL_SERVER_ERROR).body(Body::new())?);
+                return Ok(Response::from_status(500));
             }
             let geodata = geo_lookup(ip.unwrap()).unwrap();
-            Ok(Response::builder()
-                .status(StatusCode::OK)
-                .body(
-                    Body::from(
-                        serde_json::json!({
-                            "as_name": geodata.as_name(),
-                        }).to_string()
-                    )
-                )?
+            Ok(Response::new()
+                .with_status(200)
+                .with_body(
+                    serde_json::json!({
+                        "as_name": geodata.as_name(),
+                    }).to_string()
+                )
             )
         },
 
@@ -89,22 +83,22 @@ fn main(mut req: Request<Body>) -> Result<impl ResponseExt, Error> {
             use fastly::log::Endpoint;
             let mut endpoint = Endpoint::from_name("default");
             writeln!(endpoint, "Hello from fastlike!").unwrap();
-            Ok(Response::builder().status(StatusCode::NO_CONTENT).body(Body::new())?)
+            Ok(Response::from_status(StatusCode::NO_CONTENT))
         },
 
         (&Method::GET, path) if path.starts_with("/dictionary") => {
             // open the dictionary and get the key specified in the path
             let parts: Vec<&str> = path[1..].split("/").collect();
             let (name, key) = (parts[1], parts[2]);
-            use fastly::dictionary::Dictionary;
+            use fastly::Dictionary;
             let dict = Dictionary::open(name);
             let value = dict.get(key).unwrap();
-            Ok(Response::builder().status(StatusCode::OK).body(Body::from(value))?)
+            Ok(Response::new().with_status(200).with_body(value))
         },
 
-        _ => Ok(Response::builder()
-            .status(StatusCode::NOT_FOUND)
-            .body(Body::from("The page you requested could not be found"))?
+        _ => Ok(Response::new()
+            .with_status(404)
+            .with_body("The page you requested could not be found")
         ),
     }
 }
